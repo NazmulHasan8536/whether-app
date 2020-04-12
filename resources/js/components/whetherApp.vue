b4e0eece25db45c4597cae1259fd312dc1b41816
NazmulHasan8536 <nazmul.cse2019@gmail.com>
2020-04-12 22:52:29 +0600

    add Vue Files

---
+++ resources/js/components/whetherApp.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="text-white mb-8">
+    <div class="places-input text-gray-800">
+        <input type="text" class="w-full">
+
+    </div>
+
+    <div class="whether-container font-sans w-128 max-w-lg overflow-hidden bg-gray-900 shadow-lg mt-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint adipisci omnis velit, unde ratione numquam quidem quos officia reiciendis dignissimos culpa quae soluta ducimus consectetur quia laboriosam, ipsum eveniet! Sunt.
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
