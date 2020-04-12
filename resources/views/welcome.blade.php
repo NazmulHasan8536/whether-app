220764ad1c5a57042be8c9cb928fd0032f998424
NazmulHasan8536 <nazmul.cse2019@gmail.com>
2020-04-12 21:44:46 +0600

    Project setting Up & install

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+<link rel="stylesheet" href="/css/main.css">
+<script src="/js/app.js"></script>
+</head>
+<body class="bg-blue-500">
+<a href="#" class="button bg-white hover:bg-gray-500">Hello</a>
+</body>
+</html>
+
+
